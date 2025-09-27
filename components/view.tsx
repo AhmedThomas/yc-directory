33168a5345b0f09c08227cbfcab760e8bf59eafa
Ahmed Thomas <71823119+AhmedThomas@users.noreply.github.com>
2025-09-27 17:54:35 +0300

    views ui and fetch

--- components/view.tsx
+++ components/view.tsx
@@ -3,11 +3,21 @@ import Ping from './ping';
 import { client } from '@/sanity/lib/client';
 import { STARTUP_VIEWS_QUERY } from '@/sanity/lib/queries';
 import { formatViews } from '@/lib/utils';
+import { writeClient } from '@/sanity/lib/write-client';
+import { after } from 'next/server';
 
 const Views = async ({ id }: { id: string }) => {
   const { views: totalViews } = await client
     .withConfig({ useCdn: false })
     .fetch(STARTUP_VIEWS_QUERY, { id });
+
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit()
+  );
 
   return (
     <div className="view-container">
